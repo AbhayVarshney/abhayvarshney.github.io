6668472fce76e4b3845521b5e4cd1ed317f8f044
Abhay Varshney <varshneyabhay@gmail.com>
2015-06-18 18:17:48 -0700

    changes

--- js/main.js
+++ js/main.js
@@ -4,5 +4,12 @@ $(document).ready(function() {
 		$('body').addClass('loaded');
 		$('h1').css('color','#222222');
 	}, 3000);
-	
+
+	//remove page loader after 2 seconds
+ 	setTimeout(function(){
+ 		console.log("check");
+ 		var element = document.getElementById("demo-content");
+		element.outerHTML = "";
+		delete element;
+    }, 3500);
 });
