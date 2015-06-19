3b5ec52eb2905bc574d5ccf3a09ff5f8f1e9a1ff
Abhay Varshney <varshneyabhay@gmail.com>
2015-06-18 18:29:57 -0700

    change

--- js/main.js
+++ js/main.js
@@ -5,11 +5,4 @@ $(document).ready(function() {
 		$('h1').css('color','#222222');
 	}, 3000);
 
-	//remove page loader after 2 seconds
- 	// setTimeout(function(){
- 	// 	console.log("check");
- 	// 	var element = document.getElementById("demo-content");
-		// element.outerHTML = "";
-		// delete element;
-  //   }, 3250);
 });
