32f722408ffe0f11c60c15ee778b38ee5badb591
Abhay Varshney <varshneyabhay@gmail.com>
2015-06-24 12:49:35 -0700

    changes

--- js/main.js
+++ js/main.js
@@ -4,5 +4,26 @@ $(document).ready(function() {
 		$('body').addClass('loaded');
 		$('h1').css('color','#222222');
 	}, 3000);
-
 });
+
+function show(target){
+	//Remove all images before click
+	document.getElementById("timemPicture").style.display = "none";
+	document.getElementById("flappyBirdsPicture").style.display = "none";
+	document.getElementById("froggerPicture").style.display = "none";
+	document.getElementById("imageFilterPicture").style.display = "none";
+	document.getElementById("spaceInvadersPicture").style.display = "none";
+	//If user clicks...
+	if (target == "TimeM") {
+		document.getElementById("timemPicture").style.display = "";
+	} else if (target == "flappyBirds") {
+		document.getElementById("flappyBirdsPicture").style.display = "";
+	} else if (target == "frogger") {
+		document.getElementById("froggerPicture").style.display = "";
+	} else if (target == "imageFilter") {
+		document.getElementById("imageFilterPicture").style.display = "";
+	} else if (target == "spaceInvaders") {
+		document.getElementById("spaceInvadersPicture").style.display = "";
+	} else { 
+	}
+}
